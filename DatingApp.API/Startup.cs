3c4328eb5ead1c0dad5186f4614824dde8194bf7
Filipe Coelho <filipemcoelho@gmail.com>
2020-01-20 12:23:41 +0000

    End of section 5

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -15,6 +15,10 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using System.Net;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using DatingApp.API.Helpers;
 
 namespace DatingApp.API
 {
@@ -61,19 +65,35 @@ namespace DatingApp.API
         {
             // MiddleWare - Software to interact with our request through the pipeline
 
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseExceptionHandler(builder =>
+                {
+                    builder.Run(async context =>
+                                            {
+                                                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                                                var error = context.Features.Get<IExceptionHandlerFeature>();
+                                                if (error != null)
+                                                {
+                                                    context.Response.AddApplicationError(error.Error.Message);
+                                                    await context.Response.WriteAsync(error.Error.Message);
+                                                }
+                                            });
+                });
+            }
 
             // app.UseHttpsRedirection();
 
+            // Adding CORS
             app.UseCors(x => x
                                 .AllowAnyOrigin()
-                                .AllowAnyMethod()
-                                .AllowAnyHeader()
-            );
+                                    .AllowAnyMethod()
+                                    .AllowAnyHeader()
+                );
 
             app.UseRouting();
 
