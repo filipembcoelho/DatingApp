5f21dcf8be9ecd3cce2d62005370e1b07ac2d596
Filipe Coelho <filipemcoelho@gmail.com>
2020-01-24 12:34:59 +0000

    End of Section 15

--- DatingApp.API/Data/IDatingRepository.cs
+++ DatingApp.API/Data/IDatingRepository.cs
@@ -5,19 +5,15 @@ using DatingApp.API.Models;
 
 namespace DatingApp.API.Data
 {
-    public interface IDatingRepository
-    {
-        void Add<T>(T entity) where T : class;
-
-        void Delete<T>(T entity) where T : class;
-
-        Task<bool> SaveAll();
-
-        Task<PagedList<User>> GetUsers(UserParams userParams);
-
-        Task<User> GetUser(int id);
-        Task<Photo> GetPhoto(int id);
-
-        Task<Photo> GetMainPhotoForUser(int userId);
-    }
+  public interface IDatingRepository
+  {
+    void Add<T>(T entity) where T : class;
+    void Delete<T>(T entity) where T : class;
+    Task<bool> SaveAll();
+    Task<PagedList<User>> GetUsers(UserParams userParams);
+    Task<User> GetUser(int id);
+    Task<Photo> GetPhoto(int id);
+    Task<Photo> GetMainPhotoForUser(int userId);
+    Task<Like> GetLike(int userId, int recipientId);
+  }
 }
