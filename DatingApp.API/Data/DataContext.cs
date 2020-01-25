d0f9562b73a9cb0908bd8e4cd571286149d6eaaf
Filipe Coelho <filipemcoelho@gmail.com>
2020-01-25 14:09:54 +0000

    End of Section 16

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -11,24 +11,39 @@ namespace DatingApp.API.Data
     public DbSet<User> Users { get; set; }
     public DbSet<Photo> Photos { get; set; }
     public DbSet<Like> Likes { get; set; }
+    public DbSet<Message> Messages { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
 
       builder.Entity<Like>()
-          .HasKey(k => new { k.LikerId, k.LikeeId });
+              .HasKey(k => new { k.LikerId, k.LikeeId });
 
       builder.Entity<Like>()
-        .HasOne(l => l.Likee)
-        .WithMany(u => u.Likers)
-        .HasForeignKey(u => u.LikeeId)
-        .OnDelete(DeleteBehavior.Restrict);
+              .HasOne(l => l.Likee)
+              .WithMany(u => u.Likers)
+              .HasForeignKey(u => u.LikeeId)
+              .OnDelete(DeleteBehavior.Restrict);
 
       builder.Entity<Like>()
-        .HasOne(l => l.Liker)
-        .WithMany(u => u.Likees)
-        .HasForeignKey(u => u.LikerId)
-        .OnDelete(DeleteBehavior.Restrict);
+              .HasOne(l => l.Liker)
+              .WithMany(u => u.Likees)
+              .HasForeignKey(u => u.LikerId)
+              .OnDelete(DeleteBehavior.Restrict);
+
+      builder.Entity<Message>()
+              .HasOne(u => u.Sender)
+              .WithMany(m => m.MessagesSent)
+              .HasForeignKey(u => u.SenderId)
+              .OnDelete(DeleteBehavior.Restrict);
+
+      builder.Entity<Message>()
+              .HasOne(u => u.Recipient)
+              .WithMany(m => m.MessagesReceived)
+              .HasForeignKey(u => u.RecipientId)
+              .OnDelete(DeleteBehavior.Restrict);
+
+
     }
   }
 }
--- DatingApp.API/Helpers/MessageParams.cs
+++ DatingApp.API/Helpers/MessageParams.cs
@@ -1,22 +1,16 @@
 namespace DatingApp.API.Helpers
 {
-  public class UserParams
+  public class MessageParams
   {
     private const int MaxPageSize = 50;
     public int PageNumber { get; set; } = 1;
     private int _pageSize = 10;
-
     public int PageSize
     {
       get { return _pageSize; }
       set { _pageSize = (value > MaxPageSize) ? MaxPageSize : value; }
     }
     public int UserId { get; set; }
-    public string Gender { get; set; }
-    public int MinAge { get; set; } = 18;
-    public int MaxAge { get; set; } = 99;
-    public string OrderBy { get; set; }
-    public bool Likees { get; set; } = false;
-    public bool Likers { get; set; } = false;
+    public string MessageContainer { get; set; } = "Unread";
   }
 }
