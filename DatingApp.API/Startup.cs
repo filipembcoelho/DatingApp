3fbecca8e41d48950b8144745a30629436f4a7fb
Filipe Coelho <filipemcoelho@gmail.com>
2020-01-25 15:57:50 +0000

    Section 17 Production Settings on Angular

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -23,101 +23,106 @@ using AutoMapper;
 
 namespace DatingApp.API
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // Dependency Injection Container
-        // Services ethod order is not important
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddDbContext<DataContext>(
-                x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection"))
-            );
-
-
-
-            services.AddControllers().AddNewtonsoftJson(opt =>
-            {
-                opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
-            });
-
-            services.AddCors();
-
-            services.Configure<CloudinarySettings>(Configuration.GetSection("CloudinarySettings"));
-
-            services.AddAutoMapper(typeof(DatingRepository).Assembly);
-
-            services.AddScoped<IAuthRepository, AuthRepository>();
-            services.AddScoped<IDatingRepository, DatingRepository>();
-
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer(options =>
-                {
-                    options.TokenValidationParameters = new TokenValidationParameters()
-                    {
-                        ValidateIssuerSigningKey = true,
-                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Configuration.GetSection("AppSettings:Token").Value)),
-                        ValidateIssuer = false,
-                        ValidateAudience = false
-                    };
-                });
-
-            services.AddScoped<LogUserActivity>();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        // Here order is extremely important
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            // MiddleWare - Software to interact with our request through the pipeline
-
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler(builder =>
-                {
-                    builder.Run(async context =>
-                                            {
-                                                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
-                                                var error = context.Features.Get<IExceptionHandlerFeature>();
-                                                if (error != null)
-                                                {
-                                                    context.Response.AddApplicationError(error.Error.Message);
-                                                    await context.Response.WriteAsync(error.Error.Message);
-                                                }
-                                            });
-                });
-            }
-
-            // app.UseHttpsRedirection();
-
-            // Adding CORS
-            app.UseCors(x => x
-                .AllowAnyOrigin()
-                .AllowAnyMethod()
-                .AllowAnyHeader()
-            );
-
-            app.UseRouting();
-
-            app.UseAuthentication();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    // Dependency Injection Container
+    // Services ethod order is not important
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddDbContext<DataContext>(
+          x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection"))
+      );
+
+
+
+      services.AddControllers().AddNewtonsoftJson(opt =>
+      {
+        opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+      });
+
+      services.AddCors();
+
+      services.Configure<CloudinarySettings>(Configuration.GetSection("CloudinarySettings"));
+
+      services.AddAutoMapper(typeof(DatingRepository).Assembly);
+
+      services.AddScoped<IAuthRepository, AuthRepository>();
+      services.AddScoped<IDatingRepository, DatingRepository>();
+
+      services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+          .AddJwtBearer(options =>
+          {
+            options.TokenValidationParameters = new TokenValidationParameters()
+            {
+              ValidateIssuerSigningKey = true,
+              IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Configuration.GetSection("AppSettings:Token").Value)),
+              ValidateIssuer = false,
+              ValidateAudience = false
+            };
+          });
+
+      services.AddScoped<LogUserActivity>();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    // Here order is extremely important
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      // MiddleWare - Software to interact with our request through the pipeline
+
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+      else
+      {
+        app.UseExceptionHandler(builder =>
+        {
+          builder.Run(async context =>
+                                          {
+                                            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                                            var error = context.Features.Get<IExceptionHandlerFeature>();
+                                            if (error != null)
+                                            {
+                                              context.Response.AddApplicationError(error.Error.Message);
+                                              await context.Response.WriteAsync(error.Error.Message);
+                                            }
+                                          });
+        });
+      }
+
+      // app.UseHttpsRedirection();
+
+      app.UseRouting();
+
+      app.UseAuthentication();
+
+      app.UseAuthorization();
+
+      // Adding CORS
+      app.UseCors(x => x
+          .AllowAnyOrigin()
+          .AllowAnyMethod()
+          .AllowAnyHeader()
+      );
+
+      // serve the index.html in the default wwwroot folder
+      app.UseDefaultFiles();
+      app.UseStaticFiles();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+        endpoints.MapFallbackToController("Index", "Fallback");
+      });
+    }
+  }
 }
